b7dbff77fd86ca5907674a9e6f2eff776ef03a29
Unknown <may.ahmed1@hotmail.com>
2018-11-28 01:33:05 +0200

    Adjust Rotation

--- AdjustRotation.py
+++ AdjustRotation.py
@@ -2,28 +2,41 @@ import numpy as np
 import cv2
 import scipy.misc as misc
 from skimage.filters import gaussian
+from io import *
+from commonfunctions import *
 
+# Function to detect the edges of the paper to remove the effect of any light
+def paper_edge_detection(image):
+    imageCopy = image.copy()
+    imageResized = cv2.resize(src=imageCopy, dsize=(round((700 / imageCopy.shape[0]) * imageCopy.shape[1]), 700))
 
+    imageGray = cv2.cvtColor(imageResized, cv2.COLOR_RGB2GRAY)
+
+    # bilateral filter for noise removal while keeping edges sharp
+    imageBlur = cv2.bilateralFilter(imageGray, 9, 75, 75)
+    imagesThresh = cv2.adaptiveThreshold(imageBlur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 115, 4)
+    imagesThresh = cv2.medianBlur(imagesThresh, 11)
+    imagesEdDet = cv2.Canny(imagesThresh, 200, 250)
+    imagesEdDet = cv2.morphologyEx(imagesEdDet, cv2.MORPH_CLOSE, np.ones((5, 11)))
+
+    return imagesEdDet, imageResized
+
+# Function to find the bounding box around a rectangle (paper)
 def find_bounding_box(image):
-    # Convert to grayscale
-    imageGray = cv2.cvtColor(np.copy(image), cv2.COLOR_RGB2GRAY)
-    # imageGray = rgb2gray(image) * 255
 
     # Thresholding the image
-    imageThresh = np.copy(imageGray)
-    imageThresh[imageThresh > 125] = 255
-    imageThresh[imageThresh <= 125] = 0
+    imageThresh = np.copy(image)
 
-    # erosion then dilation to fill the gaps and remove the noise
-    imageErode = cv2.erode(imageThresh, np.ones((3, 3)), iterations=5)
-    imageErodDil = cv2.dilate(imageErode, np.ones((3, 3)), iterations=5)
+    # Get image with edge detection
+    imageEdgeDet, imageResized = paper_edge_detection(imageThresh)
+    io.imsave('img_edges.png', imageEdgeDet)
 
     # Get all the countours
     # cv2.RETR_TREE --> (retrieval mode) retrieves all of the contours
     # and reconstructs a full hierarchy of nested contours.
     # cv2.CHAIN_APPROX_SIMPLE -->  (contour approximation mode) compresses horizontal, vertical, and diagonal segments
     # and leaves only their end points. For example, an up-right rectangular contour is encoded with 4 points.
-    im, contours, hierarchy = cv2.findContours(imageErodDil, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    im, contours, hierarchy = cv2.findContours(np.copy(imageEdgeDet), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
     # Get the largest contour which will be the paper
     maxArea = -1
@@ -31,67 +44,67 @@ def find_bounding_box(image):
     for x in range(0, len(contours)):
         contour = contours[x]
         area = cv2.contourArea(contour)
-        if (area > maxArea):
-            maxArea = area
-            index = x
+        epsilon = 0.1 * cv2.arcLength(contour, True)
+        rect = cv2.approxPolyDP(contour, epsilon, True)
+        if(len(rect) == 4):
+            if (area > maxArea):
+                rectangle = rect
+                maxArea = area
+                index = x
     contour = contours[index]
 
-    # Get the minimum area rectangle with the biggest contour
-    rectangle = cv2.minAreaRect(contour)
-    angleRot = rectangle[-1]
-    boxRect = cv2.boxPoints(rectangle)
-    boxRect = np.int0(boxRect)
+    # Draw contour and minAreaRect on the original edge image
+    cv2.drawContours(imageResized, [rectangle], -1, (0, 255, 0), 3)
+    cv2.drawContours(imageResized, [contour], -1, (255, 0, 0), 3)
 
-    # Adjust the angle returned from minAreaRect
-    # if the width is smaller than the heignt
-    if rectangle[1][0] < rectangle[1][1]:
-        angleRot = rectangle[-1] + 180
-    else:
-        angleRot = rectangle[-1] + 90
-
-    if angleRot < 90:
-        angleRot = 90 + rectangle[-1]
-        # if the angle exceeds 90 then substract 180
-    else:
-        angleRot = angleRot - 180
-
-    # Draw contour and minAreaRect on the original image
-    imageCopy = np.copy(image)
-    cv2.drawContours(imageCopy, [boxRect], -1, (0, 255, 0), 2)
-    cv2.drawContours(imageCopy, [contour], -1, (255, 0, 0), 2)
-
-    return angleRot, boxRect, imageCopy
-
-
-# Adjust the rotation of the image
-def adjust_rotation(image):
-    # get the rotation angle and the bounding box of the paper
-    angleRot, boxRect, imageBounded = find_bounding_box(image=np.copy(image))
-    misc.imsave('boundedRot.png', imageBounded)
-
-    # rotate the image with the angle
+    # Get the points of the bounding rect
     height = image.shape[0]
     width = image.shape[1]
-    center = (width / 2, height / 2)
-    rotMatrix = cv2.getRotationMatrix2D(center, angleRot, 1.0)
-    # imageRot = cv2.warpAffine(image, rotMatrix, (width, height), flags = cv2.INTER_CUBIC, borderMode = cv2.BORDER_REPLICATE)
-    imageRot = cv2.warpAffine(image, rotMatrix, (width, height))
+    points = rectangle[:, 0]
+    x = points[:, 0]
+    y = points[:, 1]
+    points[:, 0] = x * (height / imageResized.shape[0])
+    points[:, 1] = y * (width / imageResized.shape[1])
 
-    return imageRot
+    # Get the right order of the rectangle points
+    rect = np.zeros((4, 2))
+    sum = points.sum(axis=1)
+    rect[0] = points[np.argmin(sum)]
+    rect[2] = points[np.argmax(sum)]
+    diff = np.diff(points, axis=1)
+    rect[1] = points[np.argmin(diff)]
+    rect[3] = points[np.argmax(diff)]
+
+    return rect, imageResized, maxArea
 
 
-def crop_paper(imageRot):
-    # get the bounding box of the paper on the rotated image
-    angleRot, boxRect, imageBounded = find_bounding_box(image=np.copy(imageRot))
-    misc.imsave('boundedCrop.png', imageBounded)
+# Function to adjust the rotation of the image
+def adjust_rotation(image):
+    # get the rotation angle and the bounding box of the paper
+    rect, imageBounded, maxArea = find_bounding_box(image=np.copy(image))
+    misc.imsave('boundedRot.png', imageBounded)
 
-    # Crop the image on the rotated one
-    x1 = boxRect[:, 0].min() + 100
-    y1 = boxRect[:, 1].min() + 100
-    x2 = boxRect[:, 0].max() - 100
-    y2 = boxRect[:, 1].max() - 100
-    imageCropped = imageRot[y1: y2, x1: x2]
+    if(maxArea < 2000):
+        return image
 
-    return imageCropped
+    # Get the perspective transformation of the image
+    rect = np.float32(rect)
 
+    # Calculate the dimensions of the new Image
+    width1 = np.sqrt(((rect[2][0] - rect[3][0]) ** 2) + ((rect[2][1] - rect[3][1]) ** 2))
+    width2 = np.sqrt(((rect[1][0] - rect[0][0]) ** 2) + ((rect[1][1] - rect[0][1]) ** 2))
+
+    height1 = np.sqrt(((rect[1][0] - rect[2][0]) ** 2) + ((rect[1][1] - rect[2][1]) ** 2))
+    height2 = np.sqrt(((rect[0][0] - rect[3][0]) ** 2) + ((rect[0][1] - rect[3][1]) ** 2))
+
+    width = max(int(width1), int(width2))
+    height = max(int(height1), int(height2))
+
+    # imageCorners = np.float32([[0, 0], [image.shape[1], 0], [image.shape[1], image.shape[0]], [0, image.shape[0]]])
+    imageCorners = np.float32([[0, 0], [width, 0], [width, height], [0, height]])
+
+    perspectiveTransf = cv2.getPerspectiveTransform(rect, imageCorners)
+    newImage = cv2.warpPerspective(image, perspectiveTransf, (width, height))
+
+    return newImage
 
