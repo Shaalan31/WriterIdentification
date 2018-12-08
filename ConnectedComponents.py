6368e5e965a68b2529300879b78b943e58733fc0
Ahmed Elkholy <aelkholy248@gmail.com>
2018-12-08 21:23:22 +0200

    add height to ratio feature

--- ConnectedComponents.py
+++ ConnectedComponents.py
@@ -7,20 +7,17 @@ def ConnectedComponents(contours, hierarchy, img):
     mask = (hierarchy[:, 3] == 0).astype('int')
     contours = contours[np.where(mask)]
     bounding_rect = np.zeros((len(contours), 5))
+    #average h/w
     for i in range(0, len(contours)):
         x, y, w, h = cv2.boundingRect(contours[i])
-        bounding_rect[i] = (x, y, w, h, w * h)
+        bounding_rect[i] = (int(x), int(y), int(w), int(h), int(w * h),int(h/w))
+    # h_to_w_ratio
+    h_to_w_ratio = np.average(bounding_rect[:,5],axis=0)
 
     bounding_rect_sorted = bounding_rect[bounding_rect[:, 0].argsort()]
     mask = (bounding_rect_sorted[:, 4] > 375).astype('int')
     bounding_rect_sorted = bounding_rect_sorted[np.where(mask)]
     # imgReal = cv2.imread('iAmDatabase/line1.png')
-
-    for x, y, w, h, a in bounding_rect_sorted:
-        x = int(x)
-        y = int(y)
-        w = int(w)
-        h = int(h)
         # imgReal = cv2.rectangle(imgReal,(x,y),(x+w,y+h),(0,255,0),2)
     # cv2.imwrite('iAmDatabase/boxes_img76.png', imgReal)
 
@@ -37,14 +34,10 @@ def ConnectedComponents(contours, hierarchy, img):
     total_transitions = 0
     img = img / 255
     for x, y, w, h, a in bounding_rect_sorted:
-        x = int(x)
-        y = int(y)
-        w = int(w)
-        h = int(h)
         total_transitions += np.sum(np.abs(np.diff(img[y:y + h, x:x + w])))
     total_transitions /= (2 * bounding_rect_sorted.shape[0])
     sdW = np.sqrt(np.var(bounding_rect_sorted[:, 2]))
     MedianW = np.median(bounding_rect_sorted[:, 2])
     AverageW = np.average(bounding_rect_sorted[:, 2])
 
-    return np.asarray([word_dist, within_word_dist, total_transitions, sdW, MedianW, AverageW])
+    return np.asarray([word_dist, within_word_dist, total_transitions, sdW, MedianW, AverageW,h_to_w_ratio])
